737a4b9d07e6c0c3c9bbbaac11ee2d93d65dd85a
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-01-13 20:32:35 +0100

    aave_borrow
    Added ERC20, AggregatorV3, Lending pool and Lending pool address provider interfaces to interact with aave.
    
    Interacted with the aave protocol to:
    1. Deposit weth
    2. Get user data for borrowable asset
    3. Borrowed from 2 above

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 2. Borrow some asset with the ETH collateral
    1. Sell the borrowed asset (Short selling)
 3. Repay/Pay back everything.
+
+Testing:
+Integration test: Kovan
+Unit tests: Mainnet-fork (Mock all of mainnet)
--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -15,6 +15,6 @@ def get_weth():
     # Address
     account = get_account()
     weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
-    tx = weth.deposit({"from": account, "value": Web3.toWei(0.01, "ether")})
+    tx = weth.deposit({"from": account, "value": Web3.toWei(0.1, "ether")})
     tx.wait(1)
-    print("Received 0.01 WETH")
+    print("Received 0.1 WETH")
